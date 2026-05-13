f50ff3671605233686580fdcbc88a647a3a3f645
David.Ellis <david.ellis@birmingham.gov.uk>
2026-05-13 14:58:09 +0100

    Fixed first map and made additional options

--- R/supervised-toothbrushing-map.R
+++ R/supervised-toothbrushing-map.R
@@ -7,31 +7,36 @@ library(tmap)
 # Load and process data
 school_data <- read_excel("data/supervised-toothbrushing-schools-may-2026.xlsx") %>%
   clean_names() %>%
-  # Only include schools invited
-  mutate(
-    invited_y_n == "y" 
-  ) %>%
+  # # Only include schools invited
+  # mutate(
+  #   invited_y_n == "y" 
+  # ) %>%
   # Improve status readability
   mutate(
     status = case_when(
       status == "Participating" ~ "Agreed",
       status == "Does not wish to participate" ~ "Declined",
-      is.na(status) ~ "Invited"
+      invited_y_n == "Y" ~ "Invited",
+      invited_y_n == "N" ~ "Not Invited",
+      TRUE ~ "Unexpected Status"
     ),
-    status = factor(status, levels = c("Active", "Agreed", "Declined", "Invited"))
+    status = factor(status, levels = c("Agreed", "Declined", "Invited", "Not Invited"))
   ) %>%
   # Join postcode info
   left_join(
-    # Tower Street Recreation Ground
     postcodes <- read.csv("data/West Midlands postcodes.csv") %>%
       clean_names() %>%
       filter(
         grepl("Birmingham", constituency) | constituency == "Sutton Coldfield",
       ) %>%
-      select(c(postcode, latitude, longitude)),
+      mutate(
+        IMD2019 = ceiling(10*index_of_multiple_deprivation/33755)
+      ) %>%
+      select(c(postcode, latitude, longitude, IMD2019)),
     by = join_by("postcode")
   ) %>%
-  rename(LAT = latitude, LONG = longitude, Postcode = postcode)
+  rename(LAT = latitude, LONG = longitude, Postcode = postcode) %>%
+  filter(IMD2019 %in% c(1,2))
 
 school_shape <- sf::st_as_sf(get_points_shape(school_data))
 
@@ -55,8 +60,12 @@ Brum_localities_shape <- sf::st_as_sf(Locality) %>%
 
 Brum_shape <- sf::st_union(Brum_localities_shape)
 
+################################################################################
+#                               Option 1                                       # 
+################################################################################
+
 # Load LSOA IMD data
-map <- tm_shape(Brum_shape) +
+map1 <- tm_shape(Brum_shape) +
   tm_borders(lwd=0) + 
   tm_shape(LSOA21_shape) +
   tm_fill(
@@ -67,7 +76,7 @@ map <- tm_shape(Brum_shape) +
     ),
     fill.legend = tm_legend(
       orientation = "landscape",
-      title = "IMD Decile",
+      title = "IMD Decile (2025)",
       position = tm_pos_in(0.05, 0.87),
       width = 8,
       title.size = 0.7,
@@ -98,7 +107,7 @@ map <- tm_shape(Brum_shape) +
     shape = 21,
     fill = "status",
     fill.scale = tmap::tm_scale(
-      values = c("green", "yellow", "darkred", "gray")
+      values = c("#4AA330", "#A92116", "goldenrod1", "gray")
     ),
     fill.legend = tm_legend(
       title = "Participation Status",
@@ -134,6 +143,191 @@ map <- tm_shape(Brum_shape) +
     color.light = "white"
   )
 
-map
+map1
 
-save_map(map, "output/supervised-toothbrushing-map-2026.png")
+save_map(map1, "output/option1/supervised-toothbrushing-map-2026.png")
+
+################################################################################
+#                               Option 2                                       # 
+################################################################################
+
+# All IMD 1 & 2 settings
+map2 <- tm_shape(Brum_shape) +
+  tm_borders(lwd=0) + 
+  tm_shape(
+    Brum_localities_shape
+  ) +
+  tm_borders(
+    lwd = 1,
+    col = "gray40"
+  ) +
+  tm_text(
+    "Locality",
+    col = "gray20"
+  ) +
+  tm_shape(
+    Brum_shape
+  ) +
+  tm_borders(
+    lwd = 1.5,
+    col = "gray20"
+  ) +
+  tm_shape(school_shape) +
+  tm_dots(
+    size = 0.3,
+    shape = 21,
+    fill = "#8B429E"
+  ) +
+  tmap::tm_layout(
+    legend.frame.alpha = 0,
+    legend.frame.lwd = 0,
+    legend.frame = FALSE,
+    legend.bg.alpha = 0,
+    inner.margins = c(0.08, 0.08, 0.13, 0.08),
+    frame = FALSE,
+    title = "All School Settings in Most Deprived 20% of Areas by IMD",
+    title.size = 1
+  ) +
+  tm_credits(
+    paste("Contains OS data \u00A9 Crown copyright and database right",
+          # Get current year
+          format(Sys.Date(), "%Y"),
+          ". Source:\nOffice for National Statistics licensed under the Open Government Licence v.3.0."
+    ), 
+    size = 0.8,
+    position = c(0, 0.05)
+  )+
+  tmap::tm_compass(
+    type = "8star",
+    size = 4,
+    position = c(0.75, 0.25),
+    color.light = "white"
+  )
+
+map2
+
+save_map(map2, "output/option2/imd-1and2.png")
+
+# All Invites Schools
+map3 <- tm_shape(Brum_shape) +
+  tm_borders(lwd=0) + 
+  tm_shape(
+    Brum_localities_shape
+  ) +
+  tm_borders(
+    lwd = 1,
+    col = "gray40"
+  ) +
+  tm_text(
+    "Locality",
+    col = "gray20"
+  ) +
+  tm_shape(
+    Brum_shape
+  ) +
+  tm_borders(
+    lwd = 1.5,
+    col = "gray20"
+  ) +
+  tm_shape(
+    school_shape %>%
+      filter(
+        status != "Not Invited"
+      )
+             ) +
+  tm_dots(
+    size = 0.3,
+    shape = 21,
+    fill = "#8B429E"
+  ) +
+  tmap::tm_layout(
+    legend.frame.alpha = 0,
+    legend.frame.lwd = 0,
+    legend.frame = FALSE,
+    legend.bg.alpha = 0,
+    inner.margins = c(0.08, 0.08, 0.13, 0.08),
+    frame = FALSE,
+    title = "All School Settings Invited to Participate in Supervised\nToothbrushing Programme",
+    title.size = 1
+  ) +
+  tm_credits(
+    paste("Contains OS data \u00A9 Crown copyright and database right",
+          # Get current year
+          format(Sys.Date(), "%Y"),
+          ". Source:\nOffice for National Statistics licensed under the Open Government Licence v.3.0."
+    ), 
+    size = 0.8,
+    position = c(0, 0.05)
+  )+
+  tmap::tm_compass(
+    type = "8star",
+    size = 4,
+    position = c(0.75, 0.25),
+    color.light = "white"
+  )
+
+map3
+
+save_map(map3, "output/option2/invited.png")
+
+# All Agreed Schools
+map4 <- tm_shape(Brum_shape) +
+  tm_borders(lwd=0) + 
+  tm_shape(
+    Brum_localities_shape
+  ) +
+  tm_borders(
+    lwd = 1,
+    col = "gray40"
+  ) +
+  tm_text(
+    "Locality",
+    col = "gray20"
+  ) +
+  tm_shape(
+    Brum_shape
+  ) +
+  tm_borders(
+    lwd = 1.5,
+    col = "gray20"
+  ) +
+  tm_shape(
+    school_shape %>%
+      filter(
+        status == "Agreed"
+      )
+  ) +
+  tm_dots(
+    size = 0.3,
+    shape = 21,
+    fill = "#8B429E"
+  ) +
+  tmap::tm_layout(
+    legend.frame.alpha = 0,
+    legend.frame.lwd = 0,
+    legend.frame = FALSE,
+    legend.bg.alpha = 0,
+    inner.margins = c(0.08, 0.08, 0.13, 0.08),
+    frame = FALSE,
+    title = "All School Settings that have Agreed to Participate\nin Supervised Toothbrushing Programme",
+    title.size = 1
+  ) +
+  tm_credits(
+    paste("Contains OS data \u00A9 Crown copyright and database right",
+          # Get current year
+          format(Sys.Date(), "%Y"),
+          ". Source:\nOffice for National Statistics licensed under the Open Government Licence v.3.0."
+    ), 
+    size = 0.8,
+    position = c(0, 0.05)
+  )+
+  tmap::tm_compass(
+    type = "8star",
+    size = 4,
+    position = c(0.75, 0.25),
+    color.light = "white"
+  )
+
+map4
+
+save_map(map4, "output/option2/agreed.png")
